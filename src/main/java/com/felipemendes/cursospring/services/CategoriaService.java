fe2c586b30dda6d0ff851406a037f28e5a61e418
Felipe Mendes <felipemendes1994@gmail.com>
2018-05-05 00:14:29 -0300

    Ajustes no Endpoint /categorias/{id}

--- src/main/java/com/felipemendes/cursospring/services/CategoriaService.java
+++ src/main/java/com/felipemendes/cursospring/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.felipemendes.cursospring.domain.Categoria;
 import com.felipemendes.cursospring.repositories.CategoriaRepository;
+import com.felipemendes.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,6 +14,12 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		return repo.findOne(id);
+		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+		
+		return obj;
 	}
 }
