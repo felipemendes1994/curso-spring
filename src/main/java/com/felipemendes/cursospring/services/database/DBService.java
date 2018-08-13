18bdae322c57276ffa5c20b2f38264ae572272ed
Felipe Mendes <felipemendes1994@gmail.com>
2018-08-13 14:39:50 -0300

    Salvandos Perfis de usuarios na base de dados

--- src/main/java/com/felipemendes/cursospring/services/database/DBService.java
+++ src/main/java/com/felipemendes/cursospring/services/database/DBService.java
@@ -20,6 +20,7 @@ import com.felipemendes.cursospring.domain.PagamentoComCartao;
 import com.felipemendes.cursospring.domain.Pedido;
 import com.felipemendes.cursospring.domain.Produto;
 import com.felipemendes.cursospring.domain.enums.EstadoPagamento;
+import com.felipemendes.cursospring.domain.enums.Perfil;
 import com.felipemendes.cursospring.domain.enums.TipoCliente;
 import com.felipemendes.cursospring.repositories.CategoriaRepository;
 import com.felipemendes.cursospring.repositories.CidadeRepository;
@@ -126,13 +127,20 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva","felipemendes1994@gmail.com","36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa","felipemendes1994@gmail.com","31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93888321","34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "28177012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.save(Arrays.asList(cli1));
-		enderecoRepository.save(Arrays.asList(e1, e2));
+		clienteRepository.save(Arrays.asList(cli1, cli2));
+		enderecoRepository.save(Arrays.asList(e1, e2, e3));
 		
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
